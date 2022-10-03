3d22f1810959fb0151a2ef81227ff1b428019aad
Dulat <dulat.kussainov@gmail.com>
2022-10-03 18:55:22 -0400

    table latest

--- form.js
+++ form.js
@@ -2,15 +2,6 @@ const myForm = document.querySelector('#form');
 const bottomTable = document.querySelector('#bottom-table');
 const tBody = document.querySelector('tbody');
 
-// const stonkTable = document.querySelector('#stonk-table');
-// const myPriceTable = document.querySelector('#price-table');
-// const qtyTable = document.querySelector('#qty-table');
-// const lastPriceTable = document.querySelector('#last-price-table');
-// const changeTable = document.querySelector('#change-table');
-// const changePercentTable = document.querySelector('#change-percent-table');
-// const profitTable = document.querySelector('#profit-table');
-
-
 myForm.addEventListener('submit', (e) => {
     e.preventDefault();
 
@@ -37,14 +28,12 @@ myForm.addEventListener('submit', (e) => {
     const profitTable = document.createElement('td')
     profitTable.textContent = "Profit or Loss"
 
-    
-
     tr.append(stonkTable, myPriceTable, qtyTable, lastPrice, change, percentChange, profitTable)
     tBody.append(tr)
     bottomTable.append(tBody)
     
-    
 
+    
     myForm.reset();
 
 })
