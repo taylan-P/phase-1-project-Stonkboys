383601f1633711543a6c36ff2fabe3b8857b45fd
Dulat <dulat.kussainov@gmail.com>
2022-10-04 19:32:31 -0400

    Latest Delete method

--- src/form.js
+++ src/form.js
@@ -20,6 +20,7 @@ function renderPortfolio(param) {
     const stonkTable = document.createElement('td')
     stonkTable.id = param.stonk
     stonkTable.textContent = param.stonk
+    stonkTable.dataset.id = param.id
 
     const myPriceTable = document.createElement('td')
     myPriceTable.textContent = param.purchase
@@ -28,8 +29,8 @@ function renderPortfolio(param) {
     const qtyTable = document.createElement('td')
     qtyTable.textContent = param.qty
 
-    const lastPrice = document.createElement('td')
-    lastPrice.textContent = param['last-price'].toLocaleString("en-US", {style: "currency", currency: "USD", minimumFractionDigits: 2})
+    const lastPriceTable = document.createElement('td')
+    lastPriceTable.textContent = param['last-price'].toLocaleString("en-US", {style: "currency", currency: "USD", minimumFractionDigits: 2})
 
     const change = document.createElement('td')
     change.textContent = (param['last-price'] - param.purchase).toLocaleString("en-US", {style: "currency", currency: "USD", minimumFractionDigits: 2})
@@ -49,37 +50,38 @@ function renderPortfolio(param) {
     }
     }
     tableColor()
-
+    
+    
+    //  Delete stock from my portfolio and update db.json
     const deleteBtn = document.createElement('button')
     deleteBtn.className="btn btn-warning"
     deleteBtn.innerText = 'x'
     deleteBtn.style.color = "red"
     deleteBtn.addEventListener('click', () => {
-        
+        fetch(`http://localhost:3000/myportfolio/${stonkTable.dataset.id}`, {method: 'DELETE'})
         tr.remove()
     })
 
+
+    //Refresh stock price in my portfolio and update db.json
     const refresh = document.createElement('button');
     refresh.className = "btn btn-info"
     refresh.innerText = "Refresh"
-    refresh.addEventListener('click', (e) => {
-        e.preventDefault()
+    refresh.addEventListener('click', () => {
         ticker = stonkTable.id
         fetch(`https://www.alphavantage.co/query?function=DIGITAL_CURRENCY_DAILY&symbol=${ticker}&market=USD&apikey=${apikey}`)
         .then(res => res.json())
         .then(data => {
+            debugger;
             console.log(data)
-            lastPrice.textContent = data['Time Series (Digital Currency Daily)'][lastRefreshed]['4a. close (USD)']
+            lastRefreshed = data['Meta Data']['6. Last Refreshed'].substring(0, 10)
+            lastPriceTable.textContent = data['Time Series (Digital Currency Daily)'][lastRefreshed]['4a. close (USD)']
+            change.textContent = (param['last-price'] - param.purchase).toLocaleString("en-US", {style: "currency", currency: "USD", minimumFractionDigits: 2})
             
         })
-
-        // lastPrice.textContent = parseInt(secLast.textContent).toLocaleString("en-US", {style: "currency", currency: "USD", minimumFractionDigits: 2})
-        
-
-
     })
 
-    tr.append(stonkTable, myPriceTable, qtyTable, lastPrice, change, percentChange, profitTable, deleteBtn, refresh)
+    tr.append(stonkTable, myPriceTable, qtyTable, lastPriceTable, change, percentChange, profitTable, deleteBtn, refresh)
     tBody.append(tr)
     bottomTable.append(tBody)
 
@@ -87,7 +89,6 @@ function renderPortfolio(param) {
 //Create table
 myForm.addEventListener('submit', (e) => {
     e.preventDefault();
-    debugger;
     const stonkObj = {
         "stonk": e.target.stonk.value,
         "purchase": e.target.price.value,
