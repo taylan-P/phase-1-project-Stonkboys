6a18538228363165c9cfe78499df56d08e9796e7
Chris Li <chris.lty07@gmail.com>
2022-10-03 21:35:47 -0400

    Add Refresh Button and form.js code

--- form.js
+++ form.js
@@ -1,20 +1,39 @@
 const myForm = document.querySelector('#form');
-
-const stonkTable = document.querySelector('#stonk-table');
-const myPriceTable = document.querySelector('#price-table');
-const qtyTable = document.querySelector('#qty-table');
-const lastPriceTable = document.querySelector('#last-price-table');
-const changeTable = document.querySelector('#change-table');
-const changePercentTable = document.querySelector('#change-percent-table');
-const profitTable = document.querySelector('#profit-table');
-
+const bottomTable = document.querySelector('#bottom-table');
+const tBody = document.querySelector('tbody');
 
 myForm.addEventListener('submit', (e) => {
     e.preventDefault();
+
+    const tr = document.createElement('tr');
+
+    const stonkTable = document.createElement('td')
     stonkTable.textContent = e.target.stonk.value
+
+    const myPriceTable = document.createElement('td')
     myPriceTable.textContent = e.target.price.value
+
+    const qtyTable = document.createElement('td')
     qtyTable.textContent = e.target.quantity.value
 
+    const lastPrice = document.createElement('td')
+    lastPrice.textContent = "Last price"
+
+    const change = document.createElement('td')
+    change.textContent = "Last price - Purchase price"
+
+    const percentChange = document.createElement('td')
+    percentChange.textContent = "%"
+
+    const profitTable = document.createElement('td')
+    profitTable.textContent = "Profit or Loss"
+
+    tr.append(stonkTable, myPriceTable, qtyTable, lastPrice, change, percentChange, profitTable)
+    tBody.append(tr)
+    bottomTable.append(tBody)
+    
+
+    
     myForm.reset();
 
 })
